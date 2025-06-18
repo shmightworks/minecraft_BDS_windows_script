6e76195fb52a3ad7a75f6a06a40d513cb3d4cecf
shmightworks <jasonhpchu@gmail.com>
2025-06-18 12:52:42 -0400

    Update MinecraftBDSBackupUpdate.ps1
    Updated the download latest version link grabbing, since they've changed their website.
    
    Added Suffix parameter so you can run multiple instance of minecraft server.

--- MinecraftBDSBackupUpdate.ps1
+++ MinecraftBDSBackupUpdate.ps1
@@ -1,3 +1,4 @@
+$ServerSuffix = ""
 $MinecraftServerPath = "C:\minecraft"
 $BackupPath = "C:\FTP\MinecraftBackups"
 $DTNow = get-date
@@ -23,19 +24,22 @@ $LogPath = $ScriptPath + "\logs\" + $DTNow.ToString("yyyyMMdd-HHmm") + ".log"
 if(!(Test-Path -path "$ScriptPath\Downloads")){
     New-Item -Path "$ScriptPath\Downloads" -ItemType Directory
 }
-$DownloadPath = $ScriptPath + "\Downloads\"
+$DownloadPath = $ScriptPath + "\Downloads$ServerSuffix\"
+if(!(Test-Path -path "$DownloadPath")){
+    New-Item -Path "$DownloadPath" -ItemType Directory
+}
 
 #log
 Start-Transcript -Path $LogPath
 
 #stop server
-$serverprocess = Get-Process "bedrock_server" -ErrorAction SilentlyContinue
+$serverprocess = Get-Process "bedrock_server$ServerSuffix" -ErrorAction SilentlyContinue
 if ($serverprocess) {	
 	write-host "Stopping Server" -ForegroundColor Yellow
-	Stop-Process -Force -Name "bedrock_server"  -ErrorAction SilentlyContinue
+	Stop-Process -Force -Name "bedrock_server$ServerSuffix"  -ErrorAction SilentlyContinue
 	start-sleep -s 2
 	#double check if it's still running
-	$serverprocess = Get-Process "bedrock_server" -ErrorAction SilentlyContinue
+	$serverprocess = Get-Process "bedrock_server$ServerSuffix" -ErrorAction SilentlyContinue
 }
 
 
@@ -52,15 +56,18 @@ if((Test-Path -path "$MinecraftServerPath\worlds")){
 
 #check latest version
 write-host "Checking Latest Version" -ForegroundColor Yellow
-$request = Invoke-Webrequest -Uri "https://www.minecraft.net/en-us/download/server/bedrock" -UseBasicParsing
-$download_link = $request.Links | ? href -match "bin-win/bedrock" | select -ExpandProperty href
-$latest_server_zipfile = $download_link.split("/")[4]
+#$request = Invoke-Webrequest -Uri "https://www.minecraft.net/en-us/download/server/bedrock" -UseBasicParsing
+#$download_link = $request.Links | ? class -match "btn" | ? href -match "bin-win/bedrock" | select -ExpandProperty href
+$json = Invoke-Webrequest -Uri "https://net-secondary.web.minecraft-services.net/api/v1.0/download/links" -UseBasicParsing | ConvertFrom-Json 
+$download_link = $json.result.links.Where{$_.downloadType -eq "serverBedrockWindows"}.downloadUrl
+$latest_server_zipfile = $download_link.split("/")[5]
 write-host "Latest Version: $($latest_server_zipfile)" -ForegroundColor Yellow
 
 #check if latest is downloaded
 if(!(Test-Path -path "$DownloadPath\$latest_server_zipfile")){
 	write-host "Latest Version Not Found: $($latest_server_zipfile)" -ForegroundColor Yellow
     write-host "Downloading: $($latest_server_zipfile)" -ForegroundColor Yellow
+	$ProgressPreference = 'SilentlyContinue'
     Invoke-WebRequest -Uri $download_link -OutFile "$DownloadPath\$latest_server_zipfile" -UseBasicParsing
 	
 	write-host "Backup Server" -ForegroundColor Yellow
@@ -76,13 +83,14 @@ if(!(Test-Path -path "$DownloadPath\$latest_server_zipfile")){
 	write-host "Restore Configurations" -ForegroundColor Yellow
 	Copy-Item "$ConfigBackupPath\*.json" -Destination $MinecraftServerPath -Force
 	Copy-Item "$ConfigBackupPath\*.properties" -Destination $MinecraftServerPath -Force
+	Move-Item -Path "$MinecraftServerPath\bedrock_server.exe" -Destination "$MinecraftServerPath\bedrock_server$ServerSuffix.exe" -Force
 }
 else {
 	write-host "Server Up To Date" -ForegroundColor Yellow
 }
 
 write-host "Running Server" -ForegroundColor Yellow
-Start-Process -FilePath "$MinecraftServerPath\bedrock_server.exe"
+Start-Process -FilePath "$MinecraftServerPath\bedrock_server$ServerSuffix.exe"
 
 write-host "Done" -ForegroundColor Yellow
 Stop-Transcript
